fc725ae5f0733be5234369253ff4b4ec83000897
Jude-Rizzo <jprizzo2020@gmail.com>
2019-03-06 11:29:55 -0500

    working

---
+++ bronze.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class bronze{
+  int r;
+  int c;
+  int E;
+  int N;
+
+  public int[][] stomp(int[]][]){
+
+  }
+
+  public static int[][] reader(rows, columns){
+    int[][] ans = new int[rows][cols];
+    for(int i = 1; i < rows + 1; i++){
+      for(int j = 0; j < rows + 1; j++){
+
+      }
+    }
+  }
+
+  public static void main(String[] args){
+    r = Integer.parseInt(args[0]);
+    c = Integer.parseInt(agrs[1]);
+    E = Integer.parseInt(args[2]);
+    N = Integer.parseInt(args[3]);
+  }
+}
