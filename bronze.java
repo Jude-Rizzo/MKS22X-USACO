88372669fd5c9a94ed876748ad2c7b9493f78ce6
Jude-Rizzo <jprizzo2020@gmail.com>
2019-03-06 11:44:08 -0500

    main and reader done, working on stomp

--- bronze.java
+++ bronze.java
@@ -1,27 +1,28 @@
 import java.util.*;
 public class bronze{
-  int r;
-  int c;
-  int E;
-  int N;
 
-  public int[][] stomp(int[]][]){
 
-  }
+//  public int[][] stomp(int[][] board){
 
-  public static int[][] reader(rows, columns){
+//  }
+
+  public static int[][] reader(int rows, int cols, String[] args){
+    //find length
     int[][] ans = new int[rows][cols];
-    for(int i = 1; i < rows + 1; i++){
-      for(int j = 0; j < rows + 1; j++){
-
-      }
+    int j = 4;
+    for(int n = j; n < rows * cols + 4; n++){
+      ans[(n-4)%rows][(n-4)%cols] = Integer.parseInt(args[n]);
     }
+    return ans;
   }
+
 
   public static void main(String[] args){
-    r = Integer.parseInt(args[0]);
-    c = Integer.parseInt(agrs[1]);
-    E = Integer.parseInt(args[2]);
-    N = Integer.parseInt(args[3]);
-  }
+    int r = Integer.parseInt(args[0]);
+    int c = Integer.parseInt(args[1]);
+    int E = Integer.parseInt(args[2]);
+    int N = Integer.parseInt(args[3]);
+    int[][] startingBoard = reader(r, c, args);
+}
+
 }
