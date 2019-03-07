917ab4c691eb5751b53d9567af65f4f4afaa6f4c
Jude-Rizzo <jprizzo2020@gmail.com>
2019-03-07 11:39:31 -0500

    finished setting up variables

--- bronze.java
+++ bronze.java
@@ -1,36 +1,60 @@
 import java.util.*;
+import java.io.*;
 public class bronze{
 
 
-  public int[][] stomp(int[][] board, int R, int C){
-    row = R - 1;
-    col = C - 1;
+/*  public int[][] stomp(int[][] board, int R, int C){
+    int row = R - 1;
+    int col = C - 1;
     for(int i = 0; i < 3; i++){
       for(int j = 0; j < 3; j++){
-        
+
       }
     }
   }
+  */
 
-  public static int[][] reader(int rows, int cols, String[] args){
-    //find length
-    int[][] ans = new int[rows][cols];
-    int j = 4;
-    for(int n = j; n < rows * cols + 4; n++){
-      ans[(n-4)%rows][(n-4)%cols] = Integer.parseInt(args[n]);
+
+
+
+  public static void main(String[] args) throws FileNotFoundException{
+    File f = new File(args[0]);
+    Scanner s = new Scanner(f);
+    int rows = 0;
+    int cols = 0;
+    int moves = 0;
+    int elevation = 0;
+    //pasture and N just have to be itialize to prevent compiling error, but will be changed
+    //on first instance of while loop
+    int[][] pasture = {{0,0}};
+    int[][] N = {{1,1}};
+    int lines = 0;
+    while(s.hasNextLine()){
+      if(lines == 0){
+        //use the first line of the file to initialize all of teh variables
+        rows = s.nextInt();
+        cols = s.nextInt();
+        moves = s.nextInt();
+        elevation = s.nextInt();
+        N = new int[moves][3];
+        pasture = new int[rows][cols];
+      } else if(lines > 0 && lines < rows + 1){
+        //then for the next r lines we can go through the pasture
+        for(int i = 0; i < cols; i++){
+          pasture[lines - 1][i] = s.nextInt();
+        }
+      }
+      //lastly put the instructions into our 2D array
+      else if(lines > rows){
+        for(int i = 0; i < 3; i++){
+          N[lines - (rows + 2)][i] = s.nextInt();
+        }
+      }
+
+      //add 1 + lines
+      lines ++;
+
     }
-    return ans;
-  }
-
-
-
-
-  public static void main(String[] args){
-    int r = Integer.parseInt(args[0]);
-    int c = Integer.parseInt(args[1]);
-    int E = Integer.parseInt(args[2]);
-    int N = Integer.parseInt(args[3]);
-    int[][] startingBoard = reader(r, c, args);
 }
 
 }
